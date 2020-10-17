fbc5ce278075a4d36dcc5f8ea178d12e0a0b3b6d
Jesús Agustín Peña Meza <jesusx21@gmail.com>
2020-10-17 09:26:23 -0600

    Add use case to get movies (#9)

---
+++ app/use-cases/get-movies/index.js
@@ -0,0 +1,29 @@
+const ROOT_PATH = require('app-root-path');
+const pick = require('lodash.pick');
+
+const schema = require('./schema');
+const { validateSchema } = require(`${ROOT_PATH}/utils`);
+const { ErrorGettingMovies } = require('../errors');
+
+class GetMovies {
+  constructor(data, database) {
+    this._data = data;
+    this._database = database;
+  }
+
+  async execute() {
+    await validateSchema(schema, this._data);
+
+    const filter = pick(this._data, ['sagaId', 'name', 'watched']);
+    const movies = await this._database.movies.find({ filter, limit: this._data.limit })
+      .catch();
+
+    return movies
+  }
+
+  _onUnexpectedError(error) {
+    return Promise.reject(new ErrorGettingMovies(error, this._data))
+  }
+}
+
+module.exports = GetMovies;
